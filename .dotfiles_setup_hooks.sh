b65d53a2fd0a3c43779c4b6a908200d4addd6df9
Alexey Ignatov <lexszero@gmail.com>
2015-03-13 02:44:44 +0300

    + script to setup git hooks
    That is putting shit in appropriate places at commit/checkout/merge

---
+++ .dotfiles_setup_hooks.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -x
+hd="$(git rev-parse --show-toplevel 2>/dev/null)/.git/hooks" || exit 1
+rm -f $hd/post-{commit,checkout,merge}
+cat > $hd/post-commit <<EOF
+#!/bin/bash
+sudo cp .zshrc_common /etc/zsh/zshrc_common
+sudo cp .vimrc_common /etc/vim/vimrc.local
+EOF
+for h in post-{checkout,merge}; do
+	ln -s post-commit $hd/$h
+done
